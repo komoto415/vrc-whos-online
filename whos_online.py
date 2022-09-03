b42e07c059aeb8f969779029d52a1ebea6272779
Jeffrey Ng <komoto415@gmail.com>
2022-09-03 03:04:28 -0500

    testing instances stuff

--- whos_online.py
+++ whos_online.py
@@ -1,6 +1,5 @@
-# Step 1. We begin with creating a Configuration, which contains the username and password for authentication.
 import vrchatapi
-from vrchatapi.api import authentication_api, users_api
+from vrchatapi.api import authentication_api, users_api, instances_api
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
@@ -12,29 +11,42 @@ configuration = vrchatapi.Configuration(
 
 
 with vrchatapi.ApiClient(configuration) as api_client:
-
     # Instantiate instances of API classes
     auth_api = authentication_api.AuthenticationApi(api_client)
     usr_api = users_api.UsersApi(api_client)
+    inst_api = instances_api.InstancesApi(api_client)
 
     try:
-        # Step 3. Calling getCurrentUser on Authentication API logs you in if the user isn't already logged in.
         current_user = auth_api.get_current_user()
         print("Logged in as:", current_user.display_name)
         print()
 
-        for user_id in config['LOOK_FOR'].split('\n'):
+        for user_id in config['LOOK_FOR'].strip().split('\n'):
             user = usr_api.get_user(user_id)
+            # print(user)
             print(f"User ID: {user_id}")
             print(f"Display Name:", display_name:=user.display_name)
             print(f"Username:", username:=user.username)
 
-            # State:    'active' -> On the website
+            print(f"User State:", user_state:=str(user.state))
+            # States:
+            #           'active' -> On the website
             #           'online' -> In game
-            print(f"User State:", user_state:=user.state)
-            if str(user_state) == "online":
-                print(f"User Status:", user_status:=user.status)
-                print(f"User Location:", user_location:=user.location)
+            if user_state == "online":
+                print(f"User Status:", user_status:=str(user.status))
+                # Statuses:
+                #       'join me'   -> blue
+                #       'active'    -> green
+                #       'ask me'    -> yellow
+                #       'busy'      -> red
+                print(f"Instance ID:", instance_id:=str(user.instance_id))
+                if instance_id != "private":
+                    print(f"World ID:", world_id:=str(user.world_id))
+                    # instance = inst_api.get_instance(world_id, instance_id)
+                    # print(instance)
+                    # print(f"User Location:", user_location:=user.location)
+                else:
+                    print("User Location: In Private World")
             print(f"Last Activity:", last_activity:=user.last_activity)
             print(f"Last Login:", last_login:=user.last_login)
 
